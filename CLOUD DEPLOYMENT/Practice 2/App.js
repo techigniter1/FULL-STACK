c2a1b629bd61c4c7b330843ea625f272c4021db3
Rishabh Yadav <163836298+techigniter1@users.noreply.github.com>
2025-11-12 15:01:14 +0530

    Add files via upload

---
+++ CLOUD DEPLOYMENT/Practice 2/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 CI/CD with GitHub Actions</h1>
+        <p>
+          This React app runs automated tests and builds using GitHub Actions.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
