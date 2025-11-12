a35d66e7631ac5a7426e2b8ab5495485de39e376
Rishabh Yadav <163836298+techigniter1@users.noreply.github.com>
2025-11-12 14:59:00 +0530

    Add files via upload

---
+++ CLOUD DEPLOYMENT/PRACTICE 1/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+        <p>This app is served using Nginx in a production container.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
